7075899b42c59e051e96c64db7414c012560e511
tokorom <tokorom@gmail.com>
2014-07-18 11:08:14 +0900

    First commit

---
+++ performSelector-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  performSelector-swift
+//
+//  Created by ytokoro on 7/18/14.
+//  Copyright (c) 2014 tokoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
